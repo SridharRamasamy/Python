1ee283ae74faefa5a6721f00829c562f36886328
Sridhar R <sridhar39993@gmail.com>
2022-07-29 13:41:45 +0530

    Added changes

--- Logic_first_tamil.py
+++ Logic_first_tamil.py
@@ -1,1 +1,31 @@
-VS code shortcuts
+# #  vs code shortcuts
+# # Ctrl + P
+# # >Git:
+# # Ctrl + ~
+
+
+# # Basics of python
+# # Functions - Inbuilt functions & User defined functions
+# # Inbuilt Functions
+
+# # Variables
+name = "Abinaya"
+print ("hi " + name )
+name = "John"
+print ("hi " + name)
+name = "John"        # string  - Dynamically typed language (((No need mention the data type, like int price = "45" ))) 
+price = 45           # integer  - Starts with ( letter or _) Dont use capital letters in python
+Amount = 35.5        # float
+child = True        # Boolean 
+print (child)
+
+# # # strings
+name = "John"
+name = 'John'
+name = '"John"'
+# # # inbuilt methods in strings - methods are Similar to functions
+print (name.upper())    # print() is a function & upper() is a method. Both will come with paranthesis    # method wil be called/invoked by .(dot)
+print (name.lower())  
+message ="happy birthday"
+print (message.title())
+print (message.title() + name)  # string concatenation
