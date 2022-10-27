852e65c0ebaafec2b1789f0fe1268e96a020e4e0
Sridhar R <sridhar39993@gmail.com>
2022-10-27 19:20:17 +0530

    updated

--- Freecodecamp 12 projects/3.Guess the number(by computer).py
+++ Freecodecamp 12 projects/3.Guess the number(by computer).py
@@ -18,6 +18,20 @@ computer_guess(99)
 
 # # or
 
+def computer_guess(low, high):
+    feedback = ""   # Just initialized....
+    while feedback != "c" and low != high:      # # In while loop mostly the condition will be given as not matching/failing case, so that we can get into the loop and make it pass
+        guess = random.randint(low, high)      
+        feedback = input ( f"Is {guess} too high (H), too low (L), or correct (C)??)".lower())  # C != c, H!=h 
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    print(f"Congrats, you have gueesed the number, {guess}, correctly!")        
+computer_guess(1,99)
+
+# # or
+
 import random
 def computer_guess(x):
     low =1
