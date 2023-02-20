eb972ef09163f020a8e62f328394edfa3858cd12
Sridhar R <sridhar39993@gmail.com>
2023-02-20 20:34:02 +0530

    updated

--- Durga/2.decorators.py
+++ Durga/2.decorators.py
@@ -403,9 +403,11 @@
 # # # except:
 # # #     Handling code(alternative flow)
 # # # # # # # -------------------------------------------
-print("statement-1")
-try:
-    print(10/0)
-except ZeroDivisionError:
-    print(10/2)
-print("statement-3")  
+# print("statement-1")
+# try:
+#     print(10/0)
+# except ZeroDivisionError:
+#     print(10/2)
+# print("statement-3")  
+
+
