2c7c4ff4e8ee70855fb9d53b80077cba06016a40
Sridhar R <sridhar39993@gmail.com>
2023-03-02 09:50:04 +0530

    commented

--- Projects/sudoku-tim.py
+++ Projects/sudoku-tim.py
@@ -1,33 +1,7 @@
-# # # # Naive algorithm - (Try every number in every spaces = 9^81) --> Occupies a lot of memory and time
-# # # # Backtracking algorithm 
-# # 1. Pick empty squares
-# # 2. Try all numbers
-# # 3. Find the one that works
-# # 3. Find the one that works
-# # 4. Repeat (If the solution gets wrong in the first row)
-# # 5. Backtrack and complete one row/square at once
-board = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
-]
+# # # # Python Collections (Arrays)
+# # # # There are four collection data types in the Python programming language:
 
-def print_board(bo):
-    for i in range(len(bo)):
-        if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - - - - ")
-
-        for j in range(len(bo[0])):
-            if j % 3 == 0 and j != 0:
-                print(" | ", end="")
-
-            if j == 8:
-                print(bo[i][j])
-            else:
-                print(str(bo[i][j]) + " ", end="")
+# # # # List is a collection which is ordered and changeable. Allows duplicate members.
+# # # # Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+# # # # Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+# # # # Dictionary is a collection which is ordered** and changeable. No duplicate members.
