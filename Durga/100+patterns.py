2ade528136906fc102fd88d0e26e06fc7944d7e3
Sridhar R <sridhar39993@gmail.com>
2023-03-23 18:13:55 +0530

    updated

--- Durga/100+patterns.py
+++ Durga/100+patterns.py
@@ -664,3 +664,23 @@
 # # # Number of spaces in every row: (n-i-1) & i+1
 # # # How many times to be print: i+1 & (n-i-1)
 # # # What to be print: *
+# # # -------------------------------------------------------------------------------------------------------------
+# # # Pattern - 43 
+# # To print diamond pattern with fixed digit in every row 
+# #     1
+# #    2 2
+# #   3 3 3
+# #  4 4 4 4
+# # 5 5 5 5 5
+# #  4 4 4 4
+# #   3 3 3
+# #    2 2
+# #     1
+# n = int(input("Enter the number of rows:"))
+# for i in range(n):
+#     print((n-i-1)*" "+(str(i+1)+" ")*(i+1))
+# for i in range(n-1):
+#     print(((i+1)*" ")+(str(n-i-1)+" ")*(n-i-1))
+# # # Number of spaces in every row: (n-i-1) & i+1
+# # # How many times to be print: i+1 times & (n-i-1)times
+# # # What to be print: *
